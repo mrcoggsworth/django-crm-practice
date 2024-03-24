a65d5900215629c1499a3e4f2a08bcad101b1eef
Christopher Scogin <mrcoggsworth@gmail.com>
2024-03-24 15:48:27 -0500

    registered record model

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Record(models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
