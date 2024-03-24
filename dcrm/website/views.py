c73d503a09a78a0306bc84def2b3ad26b45a8303
Christopher Scogin <mrcoggsworth@gmail.com>
2024-03-24 15:33:43 -0500

    visual updates

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -4,6 +4,9 @@
         _type_: _description_
     """
 
+from .forms import SignUpForm
+
+from typing import Any, Dict, List, Optional, Union
 
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
@@ -22,19 +25,22 @@ def home(request: HttpRequest) -> HttpResponse:
     """
 
     if request.method == "POST":
-        username: str = request.POST.get('username', default={})
-        password: str = request.POST.get('password', default={})
+        username: str = request.POST.get("username", default={})
+        password: str = request.POST.get("password", default={})
 
         # Auth
         user = authenticate(request=request, username=username, password=password)
         if user is not None:
             login(request=request, user=user)
             messages.success(request=request, message="Login Successful")
-            return redirect('home')
+            return redirect("home")
 
         if user is None:
-            messages.success(request=request, message="Oops! Something went wrong...Try again if you dare...")
-            return redirect('home')
+            messages.success(
+                request=request,
+                message="Oops! Something went wrong...Try again if you dare...",
+            )
+            return redirect("home")
 
         print("Its a POST REQUEST!")
 
@@ -79,5 +85,31 @@ def register_user(request: HttpRequest) -> HttpResponse:
     Returns:
         HttpResponse: _description_
     """
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
 
-    return render(request=request, template_name='register.html')
+            username: str = form.cleaned_data.get("username")
+            password: str = form.cleaned_data.get("password1")
+
+            user: Optional[Any | None] = authenticate(
+                username=username, password=password
+            )
+            login(request=request, user=user)
+
+            messages.success(
+                request=request, message="You have been successfully registered..."
+            )
+            return redirect(to="home")
+
+        # messages.error(request=request, message="Oops! Something went wrong...")
+        # return redirect(to="register")
+
+    if request.method == "GET":
+        form = SignUpForm()
+        return render(
+            request=request, template_name="register.html", context={"form": form}
+        )
+
+    return render(request=request, template_name="register.html", context={"form": form})
